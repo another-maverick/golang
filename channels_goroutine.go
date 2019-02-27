3ea6749f64e6c85ba2e16b9217a1ddc3cb47766b
vadlakun <vadlakun@adobe.com>
2019-02-26 21:50:16 -0700

    channels and goroutines

---
+++ channels_goroutine.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	c := make(chan int)
+
+	for outer := 0; outer <= 20; outer++ {
+		go func() {
+			for i := 0; i <= 30; i++ {
+				c <- i
+			}
+
+		}()
+	}
+
+	for j := 0; j <= 600; j++ {
+		fmt.Println(j, <-c)
+	}
+}
