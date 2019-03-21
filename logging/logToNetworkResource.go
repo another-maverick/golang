dda72f5f8494a15225c01847d1ab1012ea1c6551
Rakesh Vadlakunta <vadlakun@ITCSs-MacBook-Pro-2.local>
2019-03-21 15:18:03 -0700

    logger to network resource

---
+++ logging/logToNetworkResource.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:1234")
+
+	if err != nil {
+		panic("failed to connect on port 1234")
+	}
+
+	defer conn.Close()
+
+	logger := log.New(conn, "testGoLog  ", log.LstdFlags|log.Lshortfile)
+	logger.Println("my first log message via logger")
+	logger.Panicln("panicing now. Future logs from this program wont get printed")
+}
