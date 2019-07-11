614af6c60cd4b678c196253f36f54488bb7afa31
vadlakun <vadlakun@vadlakuns-MacBook-Pro.local>
2019-07-11 16:23:56 -0700

    parse http requests

---
+++ web-framework/parseRequest1.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+
+func headers(w http.ResponseWriter, r *http.Request) {
+	h := r.Header
+	fmt.Fprintln(w, h)
+}
+
+func main() {
+	server := http.Server{
+		Addr: "127.0.0.1:12345",
+	}
+	http.HandleFunc("/headers", headers)
+	server.ListenAndServe()
+}
+
